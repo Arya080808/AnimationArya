58d1ed6916c407766c32dd95317a10346a3bf96f
Лена <117982604+Arya080808@users.noreply.github.com>
2023-01-12 06:34:37 +0500

    AnimationArya

---
+++ AnimationArya/Animki/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Animki
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        //Random rnd = new Random();
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //string jelanie = "";
+            //closed.Source = opened.Source;
+            //int res = rnd.Next(1, 6);
+            //switch (res)
+            //{
+            //    case 1:
+            //        jelanie = "";
+            //        break;
+            //    case 2:
+            //        jelanie = "";
+            //        break;
+            //    case 3:
+            //        jelanie = "";
+            //        break;
+            //    case 4:
+            //        jelanie = "";
+            //        break;
+            //    case 5:
+            //        jelanie = "";
+            //        break;
+            //}
+            //MessageBox.Show(jelanie, "");
+            //MainWindow pictureWindow = new MainWindow();
+            //pictureWindow.Show();
+            //this.Close();
+        }
+    }
+}
